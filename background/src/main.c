fa95f5c40bd68605d4e5934154130d0717680b7f
Rubin Simons <me@rubin55.org>
2024-08-11 00:13:09 +0200

    Auto-formatted C file.

--- background/src/main.c
+++ background/src/main.c
@@ -1,12 +1,19 @@
 #include <genesis.h>
+
 #include "resources.h"
 
 int main() {
   u16 ind = TILE_USER_INDEX;
   PAL_setPalette(PAL0, bg.palette->data, DMA);
-  VDP_drawImageEx(BG_B, &bg, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, ind), 0, 0, FALSE, TRUE);
+  VDP_drawImageEx(BG_B,
+                  &bg,
+                  TILE_ATTR_FULL(PAL0, false, false, false, ind),
+                  0,
+                  0,
+                  false,
+                  DMA);
 
-  while(TRUE) {
+  while (true) {
     SYS_doVBlankProcess();
   }
 
